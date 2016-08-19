241b2a6c46282fcd6e8582d868feda1a18f84c16
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-19 13:31:55 -0500

    HAVE ADDED CLICK FUNCTION TO LOG CLICKS AND MOVE TO COPY ARRAY, CURRENTLY LOGGING TO CONSOLE LOG FOR VERFICATION

--- public/simple-simon.js
+++ public/simple-simon.js
@@ -6,68 +6,79 @@ console.log('JS linked');
 // colors need to fade out in on time out fade out
 // player then needs to click the color generated in order of the random display in order to continue
 
-var sequence = [".red", ".blue", ".green", ".yellow"];
+var sequence = ["#red", "#blue", "#green", "#yellow"];
 var copy = [];
 var elemLength = sequence.length;
+  
+
 
 // Functions for playing game 
-function flashSquare() {
+function startSequence() {
 	var random = Math.floor(Math.random() * elemLength);
 	var randomColor = sequence[random];
-	copy.push(randomColor); // push random color generated to pattern array 
+	copy.push(randomColor); // push random color generated copy array 
 	$(randomColor).animate({
 		opacity: "1"
 	}, 800).animate({
 		opacity: "0.5"
 	}, 200)
 	console.log(randomColor);
+	console.log(copy);
 }
+
+$('.box').click(function() {   // add click to buttons
+	// var item = copy.shift();
+	var buttonId = $(this).attr('id');
+	console.log(buttonId); // logging button ID to console log to verify
+});
+
 // Function for new round (still working on)
-function newRound() { 
-	// $('[data-round]').text(++this.round);
-	sequence.push(this.randomNumber());
-	copy = sequence.slice(0);
-}
+// function newRound() { 
+// // 	// $('[data-round]').text(++this.round);
+// 	sequence.push(this.randomNumber());
+// 	copy = sequence.slice(0);
+// 	console.log(copy);
+// }
 // Added function for registering clicks (still working on )
-function registerClick(e) {
-	var desiredResponse = copy.shift();
-	var actualResponse = $(e.target).data('tile');
-	correct = (desiredResponse === actualResponse);
-}
+// function registerClick(e) {
+// 	var desiredResponse = copy.shift();
+// 	var actualResponse = $(e.target).data('tile');
+// 	correct = (desiredResponse === actualResponse);
+// }
 
 // event listeners for clicks 
 $('#start').click(function() { 
 	// alert('Ready to Play?');
-	flashSquare();
+	startSequence();
 	// gamePattern();
 });
 
-$('.red').click(function() {
-	$('.red').animate({
+$('#red').click(function() {
+	$('#red').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('.blue').click(function() {
-	$('.blue').animate({
+$('#blue').click(function() {
+	$('#blue').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('.green').click(function() {
-	$('.green').animate({
+$('#green').click(function() {
+	$('#green').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('.yellow').click(function() {
-	$('.yellow').animate({
+$('#yellow').click(function() {
+	$('#yellow').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
