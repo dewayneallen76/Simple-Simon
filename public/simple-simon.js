bbc6d491f93289f4f2dc9f5a04265c9c8ca450a5
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-19 07:49:48 -0500

    CHANGED BOX COLORS TO CLASS IN HTML, UPDATED CSS TO REFLECT CHANGE. REMOVED SCORE FROM HTML CHANGED VAR BUTTONCOLORS TO SEQUENCE, I THOUGHT IT WAS AN EASIER WAY TO REMEMBER THAT I NEED TO GET A SEQUENCE FROM THAT VARIABLE. ADDED A NEW ROUND FUNCTION THAT I AM STILL WORKING ON, AND STILL WORKING ON REGISTERING CLICKS. VERFIED IN CONSOLE LOG THAT SEUQUENCE IS BEING ADDED TO THE COPY ARRAY

--- public/simple-simon.js
+++ public/simple-simon.js
@@ -6,19 +6,15 @@ console.log('JS linked');
 // colors need to fade out in on time out fade out
 // player then needs to click the color generated in order of the random display in order to continue
 
-var buttonColors = ["#red", "#green", "#yellow", "#blue"];
-var pattern = [];
-var playerPattern = [];
-var elemLength = buttonColors.length;
-// var random = Math.floor(Math.random() * elemLength);
-// var randomColor = buttonColors[random];
+var sequence = [".red", ".blue", ".green", ".yellow"];
+var copy = [];
+var elemLength = sequence.length;
 
 // Functions for playing game 
 function flashSquare() {
 	var random = Math.floor(Math.random() * elemLength);
-	var randomColor = buttonColors[random];
-	pattern.push(randomColor); // push random color generated to pattern array 
-	console.log(pattern);
+	var randomColor = sequence[random];
+	copy.push(randomColor); // push random color generated to pattern array 
 	$(randomColor).animate({
 		opacity: "1"
 	}, 800).animate({
@@ -26,20 +22,18 @@ function flashSquare() {
 	}, 200)
 	console.log(randomColor);
 }
-
-
-
-// function gamePattern() {     // plays pattern
-//     for (var i = 0; i < pattern.length; i++) {
-//       flashSquare();
-//       }
-//       console.log(pattern);
-// }
-
-
-
-
-
+// Function for new round (still working on)
+function newRound() { 
+	// $('[data-round]').text(++this.round);
+	sequence.push(this.randomNumber());
+	copy = sequence.slice(0);
+}
+// Added function for registering clicks (still working on )
+function registerClick(e) {
+	var desiredResponse = copy.shift();
+	var actualResponse = $(e.target).data('tile');
+	correct = (desiredResponse === actualResponse);
+}
 
 // event listeners for clicks 
 $('#start').click(function() { 
@@ -48,32 +42,32 @@ $('#start').click(function() {
 	// gamePattern();
 });
 
-$('#red').click(function() {
-	$('#red').animate({
+$('.red').click(function() {
+	$('.red').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('#blue').click(function() {
-	$('#blue').animate({
+$('.blue').click(function() {
+	$('.blue').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('#green').click(function() {
-	$('#green').animate({
+$('.green').click(function() {
+	$('.green').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
 	},800)
 });
 
-$('#yellow').click(function() {
-	$('#yellow').animate({
+$('.yellow').click(function() {
+	$('.yellow').animate({
 		opacity: "1"
 	}, 200).animate({
 		opacity: "0.5"
